1f350b9d64eeec3c1b16ea5cc365051a8ed24c4d
Mukhametkali <mamantayev@one.kz>
2022-04-16 00:48:58 +0600

    feat: jwt added to auth api

---
+++ auth-api/src/jwt.go
@@ -0,0 +1,48 @@
+package src
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/mukhametkaly/Diploma/auth-api/config"
+	"github.com/mukhametkaly/Diploma/auth-api/models"
+	"time"
+)
+
+type CustomClaims struct {
+	User models.User `json:"user"`
+	jwt.RegisteredClaims
+}
+
+func newToken(user models.User) (string, error) {
+	claims := CustomClaims{
+		user,
+		jwt.RegisteredClaims{
+			// A usual scenario is to set the expiration time relative to the current time
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(config.AllConfigs.SignKey)
+
+}
+
+func getAcl(token string) (user models.User) {
+
+	tk, err := jwt.ParseWithClaims(token, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.AllConfigs.SignKey), nil
+	})
+
+	if err != nil {
+		return models.User{}
+	}
+
+	if claim, ok := tk.Claims.(*CustomClaims); ok && tk.Valid {
+		return claim.User
+	} else {
+		return models.User{}
+	}
+
+}
